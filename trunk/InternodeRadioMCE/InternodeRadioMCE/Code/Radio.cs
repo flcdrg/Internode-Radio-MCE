42e084798b0e243b2c260c6f25ce3814c014aa64
david@gardiner.net.au <david@gardiner.net.au@b6f90926-37c5-a434-6863-33effbb84399>
2010-10-17 05:57:35 +0000

    Don't queue audio Custom image icon Set installation config

---
+++ trunk/InternodeRadioMCE/InternodeRadioMCE/Code/Radio.cs
@@ -0,0 +1,9 @@
+﻿namespace InternodeRadioMCE
+{
+    public class Radio
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+}
